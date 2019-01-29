6777f8ab687820c7afaccfd059e4c52921b666a7
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2019-01-29 13:54:13 +0100

    assert truth

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 int main(){
     printf("Hello world\n");
-    printf("Hanna_er_kul"); 
+    printf("Hanna_er_kul");
+    if(1 > 0){
+        return 1;
+    }
+    return 0;
     return 0;
 }
